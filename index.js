98a3d6d0047f86407661225a274d87353bc0637d
MTahir27 <m.tahiridrees27@gmail.com>
2022-08-29 06:12:32 +0500

    Project Deploy

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Node JS Server Working eoeo");
+require("dotenv").config();
+const mongoose = require("mongoose");
+try {
+  mongoose.connect(
+    "mongodb+srv://root:root@cluster0.hoz8xpg.mongodb.net/rea?retryWrites=true&w=majority"
+  );
+  console.log("Database Connection Successfull");
+} catch (error) {
+  (error) => console.log(error);
+}
