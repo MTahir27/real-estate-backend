3db2eef7a9ec42656bc9c2ab2b7b6602bc98b140
Muhammad Tahir <83488651+MTahir27@users.noreply.github.com>
2022-08-29 07:31:44 +0500

    remove url

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 try {
   mongoose.connect(
-    "mongodb+srv://root:root@cluster0.hoz8xpg.mongodb.net/rea?retryWrites=true&w=majority"
+    ""
   );
   console.log("Database Connection Successfull");
 } catch (error) {
