27eb93681ab99759c3cc844bd6909176ac08bf5d
MTahir27 <m.tahiridrees27@gmail.com>
2022-08-29 06:30:20 +0500

    Procfile Added

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ try {
 } catch (error) {
   (error) => console.log(error);
 }
+
+const express = require("express");
+const app = express();
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server Run on ${process.env.PORT || 8000} Port.`);
+});
